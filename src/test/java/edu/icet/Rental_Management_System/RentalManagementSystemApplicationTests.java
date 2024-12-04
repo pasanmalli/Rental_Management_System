1f090a2274fe815641227d9bc48be65f83d45010
pasanmalli <subasingheimal@gmail.com>
2024-12-04 10:43:06 +0530

    Rental_Management_System

---
+++ src/test/java/edu/icet/Rental_Management_System/RentalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.icet.Rental_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
